c4a9cc20701cc2102cbf35713c3516586e7db928
Joe Atzberger <ohiocore@gmail.com>
2015-03-17 16:44:12 -0700

    Refactor process_template_spec
    Must create @tmp_dir if it doesn't exist yet.
    @tmpdir is one '..' further back to get to {project root}/tmp .
    Collapse examples into a tight loop for DRYness.

--- spec/features/process_template_spec.rb
+++ spec/features/process_template_spec.rb
@@ -6,46 +6,26 @@ describe Modsulator do
     # Create common variables shared across tests and clear out temp directory
     @mods_generator = Modsulator.new
     @fixtures_dir = File.expand_path("../../fixtures", __FILE__)
-    @tmp_dir = File.expand_path("../../tmp", __FILE__)
+    @tmp_dir      = File.expand_path("../../../tmp", __FILE__)
+    Dir.mkdir(@tmp_dir) unless Dir.exists?(@tmp_dir)
     Dir.foreach(@tmp_dir) {|f| fn = File.join(@tmp_dir, f); File.delete(fn) if f != '.' && f != '..' && !File.directory?(fn)}
   end
-  
+
   describe "initialized" do
-    it "loads sample template 002 in Excel Workbook format correctly" do
-      @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
-                                               File.join(@fixtures_dir, "test_002.xlsx"),
-                                               @tmp_dir)
-      
-      expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
-      
-      expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
-      actual_result = File.read(File.join(@tmp_dir, "test:002.xml"))
-      expect(actual_result).to eq(expected_result)
-    end
-
-    it "loads sample template 002 in Excel 97-2003 Workbook format correctly" do
-      @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
-                                               File.join(@fixtures_dir, "test_002.xls"),
-                                               @tmp_dir)
-      
-      expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
-      
-      expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
-      actual_result = File.read(File.join(@tmp_dir, "test:002.xml"))
-      expect(actual_result).to eq(expected_result)
-    end
-
-    # Use LibreOffice to generate the CSV file (choose UTF-8 encoding) and ensure that any numbers are in Excel formats (decimals and such)
-    it "loads sample template 002 in CSV format correctly" do
-      @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
-                                               File.join(@fixtures_dir, "test_002.csv"),
-                                               @tmp_dir)
-      
-      expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
-      
-      expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
-      actual_result = File.read(File.join(@tmp_dir, "test:002.xml"))
-      expect(actual_result).to eq(expected_result)
+    {
+      'test_002.xlsx' => 'Excel Workbook',
+      'test_002.xls'  => 'Excel 97-2003 Workbook',
+      'test_002.csv'  => 'CSV'
+    }.each do |testfile, description|
+      it "loads sample template 002 in #{description} format correctly" do
+        @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
+                                                 File.join(@fixtures_dir, testfile),
+                                                 @tmp_dir)
+        expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
+        actual_result   = File.read(File.join(@tmp_dir, "test:002.xml"))
+        expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
+        expect(actual_result).to eq(expected_result)
+      end
     end
   end
 
