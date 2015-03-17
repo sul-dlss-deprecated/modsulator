78b2661cde77486248acfbb6090bbccc808422d5
Tommy Ingulfsen <tommyi@stanford.edu>
2015-03-17 10:04:02 -0700

    Adding tests for .xls and .csv formats.

--- spec/features/process_template_spec.rb
+++ spec/features/process_template_spec.rb
@@ -17,9 +17,34 @@ describe Modsulator do
   end
   
   describe "initialized" do
-    it "loads sample template 002 correctly" do
+    it "loads sample template 002 in Excel Workbook format correctly" do
       @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
                                                File.join(@fixtures_dir, "test_002.xlsx"),
+                                               @tmp_dir)
+      
+      expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
+      
+      expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
+      actual_result = File.read(File.join(@tmp_dir, "test:002.xml"))
+      expect(actual_result).to eq(expected_result)
+    end
+
+    it "loads sample template 002 in Excel 97-2003 Workbook format correctly" do
+      @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
+                                               File.join(@fixtures_dir, "test_002.xls"),
+                                               @tmp_dir)
+      
+      expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
+      
+      expected_result = File.read(File.join(@fixtures_dir, "test:002.xml"))
+      actual_result = File.read(File.join(@tmp_dir, "test:002.xml"))
+      expect(actual_result).to eq(expected_result)
+    end
+
+    # Use LibreOffice to generate the CSV file (choose UTF-8 encoding) and ensure that any numbers are in Excel formats (decimals and such)
+    it "loads sample template 002 in CSV format correctly" do
+      @mods_generator.generate_normalized_mods(File.join(@fixtures_dir, "SuperMods-01-template.xml"),
+                                               File.join(@fixtures_dir, "test_002.csv"),
                                                @tmp_dir)
       
       expect(File).to be_readable(File.join(@tmp_dir, "test:002.xml"))
