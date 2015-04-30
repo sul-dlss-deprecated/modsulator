1157a2f4685882da939f9c79856431c673b4bcf3
Tommy Ingulfsen <tommyi@stanford.edu>
2015-04-30 10:02:22 -0700

    Stylistic changes in response to Rubocop.

--- lib/modsulator/modsulator_sheet.rb
+++ lib/modsulator/modsulator_sheet.rb
@@ -12,12 +12,12 @@ class ModsulatorSheet
   # second argument.
   # @param [File]    file        The input spreadsheet
   # @param [String]  filename    The filename of the input spreadsheet.
-  def initialize file, filename
+  def initialize(file, filename)
     @file = file
     @filename = filename
   end
 
-  
+
   # Loads the input spreadsheet into an array of hashes. This spreadsheet should conform to the Stanford MODS template format,
   # which has three header rows. The first row is a kind of "super header", the second row is an intermediate header and the
   # third row is the header row that names the fields. The data rows are in the fourth row onwards.
@@ -28,24 +28,24 @@ class ModsulatorSheet
     # Parse the spreadsheet, automatically finding the header row by looking for "druid" and "sourceId" and leave the
     # header row itself out of the resulting array. Everything preceding the header row is discarded. Would like to use
     # clean: true here, but the latest release of Roo 1.13.2 crashes. 2.0.0beta1 seems to work though.
-    @rows ||= spreadsheet.parse(header_search: ["druid", "sourceId"]).drop(1)
+    @rows ||= spreadsheet.parse(header_search: ['druid', 'sourceId']).drop(1)
   end
 
-  
+
   # Opens a spreadsheet based on its filename extension.
   #
   # @return [Roo::CSV, Roo::Excel, Roo::Excelx]   A Roo object, whose type depends on the extension of the given filename.
   def spreadsheet
     @spreadsheet ||= case File.extname(@filename)
-    when ".csv" then Roo::Spreadsheet.open(@file, extension: :csv)
-    when ".xls" then Roo::Spreadsheet.open(@file, extension: :xls)
-    when ".xlsx" then Roo::Spreadsheet.open(@file, extension: :xlsx)
-    else raise "Unknown file type: #{@filename}"
+                     when '.csv' then Roo::Spreadsheet.open(@file, extension: :csv)
+                     when '.xls' then Roo::Spreadsheet.open(@file, extension: :xls)
+                     when '.xlsx' then Roo::Spreadsheet.open(@file, extension: :xlsx)
+                     else fail "Unknown file type: #{@filename}"
     end
   end
 
-  
-   # Get the headers used in the spreadsheet
+
+  # Get the headers used in the spreadsheet
   def headers
     rows.first.keys
   end
@@ -54,9 +54,9 @@ class ModsulatorSheet
   # Convert the loaded spreadsheet to a JSON string.
   # @return [String]  A JSON string.
   def to_json
-    json_hash = Hash.new
-    json_hash["filename"] = File.basename(filename)
-    json_hash["rows"] = rows
+    json_hash = {}
+    json_hash['filename'] = File.basename(filename)
+    json_hash['rows'] = rows
     json_hash.to_json
   end
 end
--- lib/modsulator/validator.rb
+++ lib/modsulator/validator.rb
@@ -1,6 +1,5 @@
 require 'nokogiri'
 
-
 # Validates XML against the MODSulator schema.
 class Validator
   # The Nokogiri::XML::Schema instance used for validation.
@@ -10,12 +9,13 @@ class Validator
   # @param schema_file  Full path to the desired .xsd file. If none is given, the built-in file will be used.
   def initialize(schema_file = '')
     if(schema_file == '')
-      @schema = Nokogiri::XML::Schema(File.read(File.expand_path("../modsulator.xsd", __FILE__)))
+      @schema = Nokogiri::XML::Schema(File.read(File.expand_path('../modsulator.xsd', __FILE__)))
     else
       @schema = Nokogiri::XML::Schema(File.read(schema_file))
     end
   end
 
+  
   # Validates an XML string.
   #
   # @param xml   An XML document as a string.
@@ -25,7 +25,7 @@ class Validator
     return validate_xml_doc(xml_doc)
   end
 
-
+  
   # Validates an XML document.
   #
   # @param doc   An instance of Nokogiri::XML::Document
