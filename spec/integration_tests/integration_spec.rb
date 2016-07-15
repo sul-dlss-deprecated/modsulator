45ad8b5393917693ecc1b79f545875097d24949e
Arcadia Falcone <arcadia@stanford.edu>
2016-07-15 11:11:08 -0700

    Add new test files to integration_spec.rb

--- spec/integration_tests/integration_spec.rb
+++ spec/integration_tests/integration_spec.rb
@@ -23,7 +23,8 @@ describe Modsulator do
       'crowdsourcing_bridget_2.xlsx' => 'crowdsourcing_bridget_2.xml',
       'Heckrotte_ChartsOfCoastSurvey.xlsx' => 'Heckrotte_ChartsOfCoastSurvey.xml',
       'SC1049_metadata.xlsx' => 'SC1049_metadata.xml',
-      'edition_physLoc_intmediatype.xlsx' => 'edition_physLoc_intmediatype.xml'
+      'edition_physLoc_intmediatype.xlsx' => 'edition_physLoc_intmediatype.xml',
+	  'filled_template_20160711.xlsx' => 'filled_template_20160711.xml'
     }.each do |testfile, results_file|
       it "converts #{testfile} correctly to valid XML" do
         generated_xml_string = Modsulator.new(File.join(FIXTURES_DIR, testfile), testfile).convert_rows()
