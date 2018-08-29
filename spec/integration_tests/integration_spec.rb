619ab34df44fa7ec5177fec5fb6b3569110b145a
Arcadia Falcone <arcadia@stanford.edu>
2018-08-29 11:54:53 -0700

    Update integration test to use MODS 3.6

--- spec/integration_tests/integration_spec.rb
+++ spec/integration_tests/integration_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe Modsulator do
 
   describe "generates and validates" do
     {
-      'Fitch_Chavez.xlsx' => 'Fitch_Chavez.xml',
-      'Fitch_King.xlsx' => 'Fitch_King.xml',
-      'M1463_AV_manifest.xlsx' => 'M1463_AV_manifest.xml',
-      'Matter_manifest.csv' => 'Matter_manifest.xml',
-      'PosadaSpreadsheet.xlsx' => 'PosadaSpreadsheet.xml',
-      'ars0056_manifest.csv' => 'ars0056_manifest.xml',
-      'manifest_v0174.csv' => 'manifest_v0174.xml',
-      'roman_coins_mods.xlsx' => 'roman_coins_mods.xml',
-      'crowdsourcing_bridget_1.xlsx' => 'crowdsourcing_bridget_1.xml',
-      'crowdsourcing_bridget_2.xlsx' => 'crowdsourcing_bridget_2.xml',
-      'Heckrotte_ChartsOfCoastSurvey.xlsx' => 'Heckrotte_ChartsOfCoastSurvey.xml',
-      'SC1049_metadata.xlsx' => 'SC1049_metadata.xml',
-      'edition_physLoc_intmediatype.xlsx' => 'edition_physLoc_intmediatype.xml',
-      'filled_template_20160711.xlsx' => 'filled_template_20160711.xml',
-      'location_url.xlsx' => 'location_url.xml',
-      'point_coord_test.xlsx' => 'point_coord_test.xml'
+      'Fitch_Chavez.xlsx' => '36_Fitch_Chavez.xml',
+      'Fitch_King.xlsx' => '36_Fitch_King.xml',
+      'M1463_AV_manifest.xlsx' => '36_M1463_AV_manifest.xml',
+      'Matter_manifest.csv' => '36_Matter_manifest.xml',
+      'PosadaSpreadsheet.xlsx' => '36_PosadaSpreadsheet.xml',
+      'ars0056_manifest.csv' => '36_ars0056_manifest.xml',
+      'manifest_v0174.csv' => '36_manifest_v0174.xml',
+      'roman_coins_mods.xlsx' => '36_roman_coins_mods.xml',
+      'crowdsourcing_bridget_1.xlsx' => '36_crowdsourcing_bridget_1.xml',
+      'crowdsourcing_bridget_2.xlsx' => '36_crowdsourcing_bridget_2.xml',
+      'Heckrotte_ChartsOfCoastSurvey.xlsx' => '36_Heckrotte_ChartsOfCoastSurvey.xml',
+      'SC1049_metadata.xlsx' => '36_SC1049_metadata.xml',
+      'edition_physLoc_intmediatype.xlsx' => '36_edition_physLoc_intmediatype.xml',
+      'filled_template_20160711.xlsx' => '36_filled_template_20160711.xml',
+      'location_url.xlsx' => '36_location_url.xml',
+      'point_coord_test.xlsx' => '36_point_coord_test.xml'
     }.each do |testfile, results_file|
       generated_xml_string = Modsulator.new(File.join(FIXTURES_DIR, testfile), testfile).convert_rows()
       it "converts #{testfile} to valid XML" do
